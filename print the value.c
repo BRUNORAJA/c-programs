cc1ec5ecfa604106019e9dd1c1b3a5fcde00e826
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-01-31 15:12:47 +0530

    alphabet.c

---
+++ print the value.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf("\n to print the alphabet");
+scanf("\n%d%d",&a,&b);
+if(a<=b)
+{
+printf("\n RAJA");
+}
+else
+{
+printf("\n the letter is D");
+}
+getch();
+}
