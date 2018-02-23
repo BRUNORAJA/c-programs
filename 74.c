b5dff1f43c67c655d9ebfca9eaa03becc2d1f810
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-02-23 16:01:04 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+#include<math.h>
+
+int main()
+{
+    float n;
+    
+
+    printf("Enter the number:-");
+    scanf("%f",&n);
+
+    printf("The integer number after rounded is  %5.0f",n);
+    return 0;
+    
+}
