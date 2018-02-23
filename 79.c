8b12112882e3924a855fc0cd9b2fc51388049b9d
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-02-23 16:11:34 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include<copnio.h>
+#include <math.h>
+ 
+void main()
+{
+    int num;
+    int iVar;
+    float fVar;
+    clrscr();
+  
+    printf("Enter an integer number: ");
+    scanf("%d",&num);
+  
+    fVar=sqrt((double)num);
+    iVar=fVar;
+ 
+    if(iVar==fVar)
+        printf("%d is a perfect square.",num);
+    else
+        printf("%d is not a perfect square.",num);
+      
+    getch();
+}
