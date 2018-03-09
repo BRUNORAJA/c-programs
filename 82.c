969ee7a103f2800be79d010dd73087da2731a21b
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-03-09 15:51:05 +0530

    Create 82.c

---
+++ 82.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main();
+{
+float a,b,c;
+clrscr();
+printf("\n the area of the farm");
+scanf("%f%f",&a,&b);
+c=a*b;
+printf("\n the area of the farm is %.5f",c);
+getch();
+}
