dfb69119309b0d6ac85a63cafbc84483ac4b600c
BRUNORAJA <35687885+BRUNORAJA@users.noreply.github.com>
2018-03-23 15:45:30 +0530

    Create 86.c

---
+++ 86.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include<conio.h>
+ 
+void main()
+{
+    char a, word[50];
+    int i, freq = 0, flag = 0;
+    clrscr();
+ 
+    printf("Enter character: ");
+    scanf("%c", &a);
+    printf("Now enter the word: ");
+    scanf("%s", word);
+    printf("Positions of '%c' in %s are: ", a, word);
+    for (i = 0; i < strlen(word); i++)
+    {
+        if (word[i] == a)
+        {
+            flag = 1;
+            printf("%d  ", i + 1);
+            freq++;
+        }
+    }
+    if (flag)
+    {
+        printf("\nCharacter '%c' occured for %d times.\n", a, freq);
+    }
+    else
+    {
+        printf("None\n");
+    }
+ 
+    getch();
+}
